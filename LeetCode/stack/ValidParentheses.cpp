bfb7d6156368d94b6de92c378b7479ce6a2131bd
Rishabh-5071 <rishabhdahiya652@gmail.com>
2026-01-22 22:13:36 +0530

    modified

--- LeetCode/stack/ValidParentheses.cpp
+++ LeetCode/stack/ValidParentheses.cpp
@@ -22,4 +22,26 @@ public:
         }
         return s.empty(); //returns true if string length is 0
     }
+
+    bool isValid_Stack(string s) {
+        std::stack<char> stack;
+        std::unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!stack.empty() && stack.top() == closeToOpen[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
 };
